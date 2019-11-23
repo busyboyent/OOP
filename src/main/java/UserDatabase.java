9c683862017e10bcf5e482f2bee19189ec0b13cd
Nicole <mrhomka.com@gmail.com>
2019-11-23 15:47:30 +0500

    add to userDatabase
    TryGetUserData
    TryCreateNewUserData
    ClearUserData
    SaveData

--- src/main/java/UserDatabase.java
+++ src/main/java/UserDatabase.java
@@ -4,20 +4,47 @@ import java.util.HashMap;
 import java.lang.reflect.Type;
 import com.google.gson.reflect.TypeToken;
 
+//src\\main\\resources\\userDatabase.txt
 public class UserDatabase {
     Map<String, UserData> data;
+    String dataName;
 
-    public UserDatabase(){
-
+    public UserDatabase(String file){
+        dataName = file;
+        data = InitUserDatabase();
     }
 
     public HashMap<String, UserData> InitUserDatabase(){
-        var data = SimpleReader.Read("src\\main\\resources\\userDatabase.txt");
+        var data = SimpleReader.Read(dataName);
         if (data == null) return new HashMap<>();
         Gson gson = new Gson();
         HashMap<String, UserData> userDatabase = gson.fromJson(
                 data,
                 new TypeToken<HashMap<String, UserData>>(){}.getType());
         return  userDatabase;
+    }
+
+    public UserData TryGetUserData(String user){
+        return data.get(user);
+    }
+
+    public UserData TryCreateNewUserData(String user){
+        var userData = data.get(user);
+        if (userData == null){
+            userData = new UserData();
+            data.put(user, userData);
+        }
+        return userData;
+    }
+
+    public UserData ClearUserData(String user){
+        var userData = new UserData();
+        data.put(user, userData);
+        return userData;
+    }
+
+    public void SaveData(){
+        String dataJson = new Gson().toJson(data);
+        SimpleWriter.Write(dataJson, dataName);
     }
 }
--- src/test/java/UserDatabaseTest.java
+++ src/test/java/UserDatabaseTest.java
@@ -6,16 +6,27 @@ import org.junit.Test;
 import java.util.HashMap;
 
 public class UserDatabaseTest {
+
     @Test
-    public void InitUserDatabase() {
-        Gson gson = new Gson();
-        var expect = new HashMap<String, UserData>();
-        expect.put("a", new UserData());
-        expect.get("a").SetCity("asdf");
-        String data = gson.toJson(expect);
-        SimpleWriter.Write(data, "src\\main\\resources\\userDatabase.txt");
-        var result = new UserDatabase().InitUserDatabase();
-        Assert.assertEquals(1, result.size());
-        Assert.assertEquals("asdf", result.get("a").GetCity());
+    public void TryGetUserData_Null_EmptyData() {
+        var dataBase = new UserDatabase("src\\main\\resources\\testData.txt");
+        var result = dataBase.TryGetUserData("aaaaa");
+        Assert.assertEquals(null, result);
+    }
+
+    @Test
+    public void TryCreateNewUserData_NotNull_EmptyData() {
+        var dataBase = new UserDatabase("src\\main\\resources\\testData.txt");
+        var result = dataBase.TryCreateNewUserData("aaaaa");
+        Assert.assertNotEquals(null, result);
+    }
+
+    @Test
+    public void CreateNewUserData_NotNull_NotEmptyData() {
+        var dataBase = new UserDatabase("src\\main\\resources\\testData.txt");
+        var result = dataBase.ClearUserData("aaaaa");
+        result.SetName("ssss");
+        result = dataBase.ClearUserData("aaaaa");
+        Assert.assertEquals("unknownName", result.GetName());
     }
 }
